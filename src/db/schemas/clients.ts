c161f327efa6d490a33bcf07e70b756b18b73201
Lipedev300 <luizfelipebasso22@gmail.com>
2026-01-12 11:56:04 -0300

    terminado o schema de clientes para o banco de dados, a tabela já funciona e aparece no banco, só ainda não possui dados dentro dela

--- src/db/schemas/clients.ts
+++ src/db/schemas/clients.ts
@@ -3,5 +3,6 @@ import { pgTable, uuid, varchar, text } from "drizzle-orm/pg-core";
 export const clients = pgTable("clients", {
     id: uuid("id").primaryKey().defaultRandom(),
     name: varchar("name", { length: 80}).notNull(),
+    phone: varchar("phone_number", { length: 20}).notNull().unique(),
     interests: text("interests").array()
 });
--- src/db/schemas/index.ts
+++ src/db/schemas/index.ts
@@ -1,4 +1,1 @@
 export * from "./clients";
-export * from "./restaurants";
-export * from "./tables";
-export * from "./reservations";
